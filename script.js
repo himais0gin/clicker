6906ef3ca30126143ce550fe73c2a75cb8579736
himaisogin <98646029+himais0gin@users.noreply.github.com>
2023-05-31 21:43:18 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// クリックした回数をカウントする変数
+let clickCount = 0;
+
+// ポイントを表示する要素を取得
+const pointsElement = document.getElementById("points");
+
+// クリックボタンの要素を取得
+const clickButton = document.getElementById("clickButton");
+
+// クリックイベントのリスナーを設定
+clickButton.addEventListener("click", function() {
+  // クリックした回数を増やす
+  clickCount++;
+
+  // ポイントを表示するテキストを更新
+  pointsElement.textContent = "ポイント: " + clickCount;
+});
